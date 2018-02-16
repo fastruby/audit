a6a403dc5d6ee3bbdfe6ce836b4ec8afa35cea98
ercohen14 <ercohen14@gmail.com>
2018-02-16 13:26:38 -0500

    Add AWS credentials

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem "paperclip", "~> 5.2.1"
+gem 'aws-sdk', '~> 2.3.0'
 gem 'pg', '~> 0.8'
 
 group :development, :test do
--- app/models/gemfile.rb
+++ app/models/gemfile.rb
@@ -1,4 +1,12 @@
 class Gemfile < ApplicationRecord
-  has_attached_file :file, default_url: "/images/:style/missing.png"
+  has_attached_file :file, default_url: "/images/:style/missing.png",
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
+
+  def s3_credentials
+    { :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  end
 end
