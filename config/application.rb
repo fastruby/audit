026c24b0e49c024f0df4d62a800eb4586a76687c
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2018-02-27 23:01:48 -0500

    Configure paperclip per environment

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module VulnerableGems
   class Application < Rails::Application
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        bucket: ENV['AWS_BUCKET'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      },
+      s3_region: "us-east-1"
+    }
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
