1ed075ba1478b121ed8250115aa3b7e19229abea
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2018-02-27 23:01:21 -0500

    Implement check_with_bundler_audit

--- app/models/gemfile.rb
+++ app/models/gemfile.rb
@@ -1,23 +1,46 @@
+require "bundler/audit/scanner"
+
 class Gemfile < ApplicationRecord
-  has_attached_file :file, default_url: "/images/:style/missing.png",
-                    :storage => :s3,
-                    :s3_credentials =>{
-                      :bucket => ENV['AWS_BUCKET'],
-                      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                    },
-                    :s3_region => "us-east-1"
+  has_attached_file :file
   # Validate content type
-  validates_attachment_content_type :file, :content_type => /\Atext/
+  validates_attachment_content_type :file, content_type: /\Atext/
 
   # Validate filename
-  validates_attachment_file_name :file, :matches => [/lock\Z/]
+  validates_attachment_file_name :file, matches: [/lock\Z/]
 
   # Explicitly do not validate
   # do_not_validate_attachment_file_type :avatar
   # validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
 
   def check_with_bundler_audit
-    # code goes here
+    result = {
+      warnings: [], advisories: {}
+    }
+    #
+    scanner = Bundler::Audit::Scanner.new(
+      File.dirname(file.path),
+      File.basename(file.path)
+    )
+
+    scanner.scan do |insp|
+      case insp
+      when Bundler::Audit::Scanner::InsecureSource
+        result[:warnings] << "Insecure Source URI found: #{insp.source}"
+      when Bundler::Audit::Scanner::UnpatchedGem
+        gem = insp.gem
+        advisory = insp.advisory
+
+        result[:advisories]["#{gem.name}@#{gem.version}"] = {
+          name: gem.name,
+          version: gem.version.to_s,
+          id: advisory.id,
+          url: advisory.url,
+          title: advisory.title,
+          description: advisory.description
+        }
+      end
+    end
+
+    result
   end
 end
