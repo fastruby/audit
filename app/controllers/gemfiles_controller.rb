baf3b26610486f00ab2855aef860db4268f1df34
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2018-03-13 22:54:04 -0400

    Use alpha_id to find the gemfiles
    Add spec to cover that new behavior

--- app/controllers/gemfiles_controller.rb
+++ app/controllers/gemfiles_controller.rb
@@ -3,7 +3,7 @@ class GemfilesController < ApplicationController
     @file = Gemfile.new(gemfile_params)
 
     if @file.save
-      redirect_to gemfile_path(@file), notice: "Gemfile successfully imported"
+      redirect_to gemfile_path(@file.alpha_id), notice: "Gemfile successfully imported"
     else
       redirect_to root_path, error: "There was a problem"
     end
@@ -12,7 +12,7 @@ class GemfilesController < ApplicationController
   def show
     @renderer = Redcarpet::Render::HTML.new(prettify: true)
     @markdown = Redcarpet::Markdown.new(@renderer, fenced_code_blocks: true)
-    @file = Gemfile.find(params[:id])
+    @file = Gemfile.find_by!(alpha_id: params[:id])
     @vulnerabilities = @file.check_with_bundler_audit
   end
 
---
+++ spec/controllers/gemfiles_controller_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe GemfilesController do
+  render_views
+
+  let(:file) do
+    File.new("#{Rails.root}/spec/support/fixtures/#{gemfile_lock}")
+  end
+  let(:gemfile_lock) do
+    "healthy_Gemfile.lock"
+  end
+  subject { Gemfile.create(file: file) }
+
+  describe "#show" do
+    context "when trying to find gemfile by id" do
+      it "won't find any" do
+        expect do
+          get :show, params: { id: subject.id }
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "when trying to find gemfile by alpha id" do
+      let(:message) { "No vulnerabilities found!" }
+
+      it "will find it" do
+        get :show, params: { id: subject.alpha_id }
+
+        expect(response).to be_ok
+        expect(response.body).to include(message)
+      end
+    end
+  end
+
+end
