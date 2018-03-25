87c92f65ad57cde8380d27b31fe7a12c8bf8467a
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2018-03-25 10:41:28 -0400

    Renamed migration file and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314023556) do
+ActiveRecord::Schema.define(version: 20180325143727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
