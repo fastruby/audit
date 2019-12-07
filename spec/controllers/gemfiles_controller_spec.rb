fb76f57dcc63a5d13d6a45a683cdb3bc0f049861
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-12-06 21:15:41 -0500

    Add more coverage for (gemfiles#create)

--- spec/controllers/gemfiles_controller_spec.rb
+++ spec/controllers/gemfiles_controller_spec.rb
@@ -1,14 +1,52 @@
 require "rails_helper"
 
-RSpec.describe GemfilesController do
+RSpec.describe GemfilesController, type: :controller do
   render_views
 
-  let(:file) do
-    File.new("#{Rails.root}/spec/support/fixtures/Gemfile.lock")
+  describe "#create" do
+    context "when file is empty" do
+      let(:msg) do
+        "File can't be blank"
+      end
+
+      it "returns an error message" do
+        post :create
+
+        expect(flash[:error]).to eq(msg)
+      end
+    end
+
+    context "when Gemfile.lock is invalid" do
+      let(:file) do
+        File.new("#{Rails.root}/spec/support/fixtures/corrupt.lock")
+      end
+      let(:file_upload) do
+        fixture_file_upload(file)
+      end
+      let(:msg) do
+        "File has contents that are not what they are reported to be. File is invalid. File content type is invalid"
+      end
+
+      before do
+        post :create, params: { gemfile: { file: file_upload } }
+      end
+
+      it "redirects to the homepage" do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "loads an error message in flash[:error]" do
+        expect(flash[:error]).to eq(msg)
+      end
+    end
   end
-  subject { Gemfile.create(file: file) }
 
   describe "#show" do
+    let(:file) do
+      File.new("#{Rails.root}/spec/support/fixtures/Gemfile.lock")
+    end
+    subject { Gemfile.create(file: file) }
+
     context "when trying to find gemfile by id" do
       it "won't find any" do
         expect do
