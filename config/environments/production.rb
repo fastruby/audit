476d055003adc7445bdb6606d6bae126d3a385c3
Julio Lucero <poramo@gmail.com>
2026-07-28 12:37:54 -0300

    Cache static files and lengthen audit tool title tag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{1.week.to_i}"
+  }
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :terser
   # config.assets.css_compressor = :sass
